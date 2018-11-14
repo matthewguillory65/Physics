d0e8e2a483cb5e4c24e632e05a372ecb6b55f8a7
matthewguillory65 <matthewguillory65@gmail.com>
2018-11-13 18:03:32 -0600

    Changes and adds

---
+++ Physics/Assets/AABB/Scripts/CollisionBehavior.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace AABB
+{
+    public class CollisionBehavior : MonoBehaviour
+    {
+        public Vector2 minXY;
+        public Vector2 maxXY;
+
+        public bool isColliding = false;
+
+        [NonSerialized]
+        List<CollisionBehavior> XYmin;
+        [NonSerialized]
+        List<CollisionBehavior> XYmax;
+
+        private void Start()
+        {
+
+        }
+
+        public void AddToList()
+        {
+            
+        }
+
+        public void RemoveFromList()
+        {
+            
+        }
+        
+        public void Collide()
+        {
+
+        }
+    }
+}
+
+
