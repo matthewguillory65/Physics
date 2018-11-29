b787e7bfc422eed3d3ebe63f98c3a4780336eae6
matthewguillory65 <matthewguillory65@gmail.com>
2018-11-29 17:40:13 -0600

    Work
    Forgot to push Tuesday. Making the Cloth portion of the assessment.

---
+++ Physics/Assets/SpringDamper/Scripts/CustomMeshGenerator.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomMeshGenerator : MonoBehaviour
+{
+    List<Vector3> Vertices = new List<Vector3>();
+    List<int> TrianglePoints = new List<int>();
+    List<Vector3> SurfaceNormals = new List<Vector3>();
+    List<Vector2> UVs = new List<Vector2>();
+
+    public MeshFilter InstanceMeshFilter;
+    public Mesh InstanceMesh;
+
+	// Use this for initialization
+	void Start ()
+    {
+        InstanceMesh = new Mesh();
+        InstanceMesh.name = "Mesh";
+
+        for (int x = 0; x < 5; x++)
+        {
+            for (int y = 0; y < 5; y++)
+            {
+                Vertices.Add(new Vector3(x, y, 0));
+            }
+        }
+        InstanceMesh.vertices = Vertices.ToArray();
+
+        for (int i = 0; i < Vertices.Count - 5; i++)
+        {
+            if (i % 5 != 5 - 1 && i < Vertices.Count - 5)
+            {
+                //Bottom Triangle
+                TrianglePoints.Add(i);//bot left
+                TrianglePoints.Add(i + 1);//bot right
+                TrianglePoints.Add(i + 5);//top left
+
+                //Top Triangle
+                TrianglePoints.Add(i + 1);//bot right
+                TrianglePoints.Add(i + 5 + 1);//top right
+                TrianglePoints.Add(i + 5);//top left
+            }
+        }
+        InstanceMesh.triangles = TrianglePoints.ToArray();
+
+
+        foreach(var vert in Vertices)
+        {
+            SurfaceNormals.Add(new Vector3(0, 0, 1));
+        }
+        InstanceMesh.normals = SurfaceNormals.ToArray();
+
+        foreach(var vert in Vertices)
+        {
+            UVs.Add(new Vector2(vert.x / (5 - 1), vert.y / (5 - 1)));
+        }
+        InstanceMesh.uv = UVs.ToArray();
+
+        InstanceMeshFilter.mesh = InstanceMesh;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ Physics/Assets/SpringDamper/Scripts/SpringDamper.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringDamper
+{
+    Particle pOne; //Particle One
+    Particle pTwo; //Particle Two
+    float Ks; //Spring Constant
+    float Kd; //Damping Factor
+    float Lo; //Rest Length
+    float L; //Length
+    //Properties of a spring damper
+
+    public SpringDamper(Particle p1, Particle p2)
+    {
+        Ks = 50;
+        Kd = 2;
+        Lo = Vector3.Distance(p1.r, p2.r);
+        pOne = p1;
+        pTwo = p2;
+    }
+    
+
+    void Update()
+    {
+        //Moving the Particle
+        var ePrime = pTwo.r - pOne.r;
+        var eMag = ePrime.magnitude;
+        var e = ePrime / eMag;
+        e.Normalize();
+
+        var v1 = Vector3.Dot(e, pOne.v);
+        var v2 = Vector3.Dot(e, pTwo.v);
+
+        var Fsd = -Ks * (Lo - L) - Kd * (v1 - v2);
+        var F1 = Fsd * e;
+        var F2 = -F1;
+    }
+}
+
+
+public class Particle
+{
+    public readonly Vector3 r; //Position
+    Vector3 rNew; //New Position
+    public Vector3 v; //Velocity
+    Vector3 vNew; //New Velocity
+    Vector3 a; //Acceleration
+    float m = 1f; //Mass
+    Vector3 f; //Force
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Acceleration
+        a = f * m;
+        //New Velocity
+        vNew = v + (a * Time.deltaTime);
+        //New Position
+        rNew = r + (v * Time.deltaTime);
+    }
+}
